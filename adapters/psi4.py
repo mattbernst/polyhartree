a7e26657ef1c3ce0ffd055d8f5175ca09d267e8f
Matt Ernst <mbernst@tricity.wsu.edu>
2015-04-19 21:45:38 -0700

    Added basic Psi4 scf energy support.

---
+++ adapters/psi4.py
@@ -0,0 +1,305 @@
+# -*- coding:utf-8 mode:python; tab-width:4; indent-tabs-mode:nil; py-indent-offset:4 -*-
+
+import hashlib
+import os
+import uuid
+import cpinterface
+
+class Psi4Job(cpinterface.Job):
+    def __init__(self, *args, **kw):
+        super(Psi4Job, self).__init__(*args, **kw)
+        self.backend = "psi4"
+
+    def extract_last_energy(self, data, options={}):
+        """Get last energy message from log file and store it as energy.
+
+        @param data: log file contents
+        @type data : str
+        @param options: ignored
+        @type options : dict
+        """
+
+        for line in data.split("\n"):
+            if "Final Energy" in line:
+                energy = self.n_number_from_line(line, 0, 1)
+
+                #units are already Hartree
+                self.energy = energy
+
+    def run_local(self, options={}):
+        """Run a Psi4 job using psi script, on the local host.
+
+        @param options: ignored
+        @type options : dict
+        """
+        
+        workdir = self.backend + "-" + str(uuid.uuid1()).replace('-', '')[:16]
+        path = "/tmp/{0}/".format(workdir)
+        os.makedirs(path)
+
+        deck_hash = hashlib.sha1(self.deck).hexdigest()[:10]
+        dat_file = "{0}.dat".format(deck_hash)
+        abs_file = path + dat_file
+        log_file = abs_file.replace(".dat", ".log")
+        with open(abs_file, "w") as deckfile:
+            deckfile.write(self.deck)
+
+        #N.B. psi4 build from git is currently screwed up and anaconda tries
+        #to take over all things Python-related... for now, hack around by
+        #activating anaconda for psi4 binary package only
+        cmd = "source ~/.bashrc-anaconda && cd {0} && psi4 -i {1} -o {2}".format(path, dat_file, log_file)
+        
+        stdout, returncode = self.execute(cmd, cwd=path, bash_shell=True)
+        self.stdout = stdout
+
+        with open(log_file, "r") as lfile:
+            self.logdata = lfile.read()
+
+            if "PsiException:" in self.logdata:
+                self.runstate = "error"
+                
+            else:
+                self.extract_last_energy(self.logdata)
+                self.runstate = "complete"
+
+class Psi4(cpinterface.MolecularCalculator):
+    def __init__(self, *args, **kw):
+        super(Psi4, self).__init__(*args, **kw)
+        
+        self.methods = ["hf:rhf", "hf:uhf", "hf:rohf"]
+        self.coordinate_choices = ["cartesian", "zmatrix"]
+        self.references = ["RHF", "ROHF", "UHF"]
+
+    def make_tagged_cartesian_geometry(self, system, options={}):
+        """Create a geometry block using tagged atom names.
+
+        options:
+         property_name: name of tag group to use from atom_properties
+
+        @param system: molecular system data to convert to input geometry
+        @type system : geoprep.System
+        @return: atom coordinates named by atom tags
+        @rtype : list
+        """
+
+        tag_name = options["property_name"]
+        tags = system.atom_properties(tag_name)
+
+        entries = []
+        for j, tag in enumerate(tags):
+            atom = system.atoms[j]
+            entry = "{}\t{:.6f}\t{:.6f}\t{:.6f}".format(tag, atom.coords[0],
+                                                        atom.coords[1],
+                                                        atom.coords[2])
+            entries.append(entry)
+
+        return entries
+
+    def create_geometry(self, system, options={}):
+        """Create input geometry for a subsequent calculation.
+
+        options:
+         coordinates: "cartesian" or "zmatrix"
+         symmetry: optional symmetry group
+
+        @param system: molecular system data to convert to input geometry
+        @type system : geoprep.System
+        @param options: select coordinate system
+        @type options : dict
+        @return: a Psi4 input with geometry specifications
+        @rtype : str
+        """
+
+        defaults = {"coordinates" : "cartesian"}
+        options = dict(defaults.items() + options.items())
+        symmetry = options.get("symmetry")
+        
+        coord_choice = self.check_coordinates(options.get("coordinates"))
+
+        if coord_choice == "cartesian":
+            coordinates = self.make_tagged_cartesian_geometry(system,
+                                                              options=options)
+            head = "molecule"
+            #psi4 reads charge and multiplicity from geometry block
+            chargemult = "{0} {1}".format(system.charge, system.spin)
+            directives = [head, "#charge multiplicity", chargemult]
+            if symmetry:
+                directives.append("symmetry {0}".format(symmetry))
+            geometry = self.make_control_block(directives + coordinates)
+
+        elif coord_choice == "zmatrix":
+            raise ValueError("Psi4 zmatrix input currently unsupported")
+
+        return geometry
+
+    def make_control_block(self, controls, indent="  "):
+        """Make a textual control block for Psi4. For example, to modify
+        assign basis parameters, pass in
+        ["basis", "assign H1 sto-3g" "assign C1 sto-3g"] as
+        controls and a block like this will be generated:
+
+        basis {
+          assign H1 sto-3g
+          assign C1 sto-3G
+        }
+
+        @param controls: block name followed by parameters to insert in block
+        @type controls : list
+        @param indent: whitespace to prepend to block parameters
+        @type indent : str
+        @return: formatted control block
+        @rtype : str
+        """
+
+        lines = [controls[0] + " {"]
+        for control in controls[1:]:
+            line = indent + control
+            lines.append(line)
+
+        lines.append("}")
+        block = "\n".join(lines)
+        return block
+
+    def make_energy_job(self, system, method, options={}):
+        """Create an input specification for a single point energy calculation.
+
+        @param system: molecular system for energy calculation
+        @type system : geoprep.System
+        @param method: calculation method
+        @type method : str
+        @param options: additional keyword based control options
+        @type options : dict
+        @return: a Psi4 input for single point energy calculation
+        @rtype : str
+        """
+
+        system = self.fragment_to_system(system)
+
+        self.check_method(method)
+
+        if method.startswith("hf"):
+            return self.make_hf_job(system, method, "ENERGY", options=options)
+
+        else:
+            raise ValueError("Psi4 does not currently support {0}".method)
+
+    def prepare_basis_data(self, system, options={}):
+        """Prepare basis set data for use: basis assignment, inline
+        basis specifications, and information for spherical/cartesian flag.
+
+        Also assign basis tags to atoms as atom properties.
+
+        @param system: molecular system for parameterization
+        @type system : geoprep.System
+        @param options: additional keyword based control options
+        @type options : dict
+        @return: basis set control block
+        @rtype : dict
+        """
+
+        property_name = options["basis_tag_name"]
+        ubb = {}
+        basis_names = []
+        basis_blocks = []
+        assignments = []
+        bsd = self.get_basis_data(system, {"basis_format" : "g94"})
+        data = bsd["data"]
+
+        form = bsd["spherical_or_cartesian"]
+
+        symbols = system.atom_properties("symbols")
+        for j, basis_name in enumerate(system.atom_properties("basis_name")):
+            add_new_block = False
+            element = symbols[j]
+            if element not in ubb:
+                ubb[element] = []
+                
+            basis_element = "{0}:{1}".format(basis_name, element)
+
+            #this (basis, element) pair is new, so add it
+            if basis_element not in ubb[element]:
+                ubb[element].append(basis_element)
+                add_new_block = True
+
+            basis_index = ubb[element].index(basis_element)
+            basis_alias = "{0}{1}".format(element, basis_index)
+            basis_names.append(basis_alias)
+            
+            if add_new_block:
+                assignment = "assign {0} {1}".format(basis_alias, basis_alias)
+                assignments.append(assignment)
+                basis_text = data[basis_name][element]
+
+                #add basis data with comment e.g. "H 3-21G"
+                #also indicate cartesian/spherical form
+                comment = "#{0} {1}".format(element, basis_name)
+                designation = "[ {0} ]".format(basis_alias)
+                basis_lines = [comment, designation, form, basis_text]
+                                    
+                block = "\n".join(basis_lines)
+                
+                basis_blocks.append(block)
+
+        blocks = "\n".join(basis_blocks)
+        formatted = blocks
+
+        data = self.make_control_block(["basis"] + assignments +
+                                       blocks.split("\n"))
+
+        #set basis tag for each atom
+        system.set_properties(property_name, range(len(system.atoms)),
+                              basis_names)
+
+        r = {"basis_data" : data}
+        
+        return r
+
+    def make_hf_job(self, system, method, runtyp, options={}):
+        """Create an input specification for a Hartree-Fock calculation.
+        
+        Psi4 supports RHF restricted Hartree-Fock,
+        UHF unrestricted Hartree-Fock, and
+        ROHF restricted open shell Hartree-Fock
+
+        @param system: molecular system for calculation
+        @type system : geoprep.System
+        @param method: a HF calculation method
+        @param options: additional keyword based control options
+        @type options : dict
+        @type method : str
+        @return: a Psi4 HF job
+        @rtype : Job
+        """
+
+        defaults = {"reference" : "rhf", "scf_iterations" : 999,
+                    "basis_tag_name" : "basis_tag",
+                    "property_name" : "basis_tag"}
+        options = dict(defaults.items() + options.items())
+
+        self.check_method(method)
+
+        bd = self.prepare_basis_data(system, options=options)
+        geometry = self.create_geometry(system, options=options)
+        
+        method = method.split("hf:")[-1].upper()
+
+        reference = options.get("reference").upper()
+        self.check_electronic_reference(reference or options["reference"].upper())
+        if system.spin > 1 and reference == "RHF":
+            self.log("Forcing UHF for multiplicity {0}".format(system.spin))
+            reference = "UHF"
+
+        gb = ["set globals", "reference {0}".format(reference),
+              "maxiter {0}".format(options.get("scf_iterations"))]
+        gb = self.make_control_block(gb)
+
+        deck = ["# {0}".format(system.title),
+                geometry,
+                gb,
+                bd["basis_data"],
+                "energy('scf')"]
+        
+        deck = "\n\n".join(deck)
+        
+        job = Psi4Job(deck=deck, system=system)
+        return job
---
+++ tests/test_psi4.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 mode:python; tab-width:4; indent-tabs-mode:nil; py-indent-offset:4 -*-
+##
+
+"""
+    test_psi4
+    ~~~~~~~~~~~~~~
+
+    Test Psi4 implementations for energy, minimum geometry,
+    transition state geometry, and frequencies.
+"""
+
+import sys
+import unittest
+import geoprep
+from adapters import psi4
+from sharedutilities import Utility
+
+class Psi4TestCase(unittest.TestCase):
+    def setUp(self):
+        self.G = geoprep.Geotool()
+        self.C = psi4.Psi4()
+
+    def tearDown(self):
+        pass
+
+    def test_make_control_block(self):
+        #test control block formatting
+        expected = "molecule {\n  0 1\n  H1 0.0 0.0 0.0\n  H2 0.9 0.0 0.0\n}"
+        cartesians = ["molecule", "0 1", "H1 0.0 0.0 0.0", "H2 0.9 0.0 0.0"]
+        block = self.C.make_control_block(cartesians)
+        self.assertEqual(expected, block)
+
+    def test_energy_scf_methane(self):
+        #very basic minimal basis set test for methane
+        methane = self.G.make_fragment("C")
+        methane.set_basis_name("3-21G")
+        job = self.C.make_energy_job(methane, "hf:rhf")
+        job.run_local()
+        self.assertAlmostEqual(-39.976642, job.energy, places=5)
+
+    def test_energy_rohf_uhf_scf_methane(self):
+        #compare UHF and ROHF across methyl radicals for HF energy
+        
+        smiles = {"methyl_radical" : "[CH3]"}
+        jobs = {}
+
+        for key in smiles:
+            s = smiles[key]
+            fragment = self.G.make_fragment(s)
+            fragment.set_basis_name("cc-pVDZ")
+
+            for reference in ["uhf", "rohf"]:
+                j = self.C.make_energy_job(fragment,
+                                           "hf:{0}".format(reference))
+                j.run_local()
+                name = "{0}-{1}".format(key, reference)
+                jobs[name] = j
+
+        energies = dict([(j, jobs[j].energy) for j in jobs])
+
+        #these don't match NWChem/GAMESS to 6 decimal places. Tighter SCF
+        #convergence needed?
+        self.assertAlmostEqual(-39.5638189, energies["methyl_radical-rohf"],
+                               places=6)
+        self.assertAlmostEqual(-39.5638189, energies["methyl_radical-uhf"],
+                               places=6)
+
+    def test_bad_input_error(self):
+        methane = self.G.make_fragment("C")
+        methane.set_basis_name("3-21G")
+        job = self.C.make_energy_job(methane, "hf:rhf")
+
+        #introduce an error in the input deck: misspell scf as sfc
+        job.deck = job.deck.replace("scf", "sfc")
+
+        self.assertEqual("begin", job.runstate)
+        job.run_local()
+
+        self.assertEqual("error", job.runstate)
+
+    def test_prepare_basis_data(self):
+        #test generation of inline basis set data with hydrogen peroxide
+        #should generate one basis set assignment for oxygen and two for
+        #hydrogen, since one hydrogen gets assigned a different basis
+        btag = "basis_tag"
+        expected_labels = ["H0", "H1", "O0"]
+        expected_tags = ["O0", "O0", "H0", "H1"]
+
+        peroxide = self.G.make_fragment("OO")
+        peroxide.set_basis_name("3-21G")
+        hydrogens = peroxide.select("[O]", hydrogen="only")
+        peroxide.set_basis_name("6-31G", hydrogens[:1])
+        
+        s = geoprep.System([peroxide])
+        b = self.C.prepare_basis_data(s, options={"basis_tag_name" : btag})
+
+        for label in expected_labels:
+            self.assertTrue(label in b["basis_data"])
+
+        basis_tags = s.atom_properties(btag)
+        self.assertEqual(expected_tags, basis_tags)
+
+    def test_make_tagged_cartesian_geometry(self):
+        #test generation of cartesian coordinates matched to atom tags
+        btag = "basis_tag"
+        options = {"basis_tag_name" : btag, "property_name" : btag}
+        expected_tags = ["O0", "O0", "H0", "H1"]
+
+        peroxide = self.G.make_fragment("OO")
+        peroxide.set_basis_name("3-21G")
+        hydrogens = peroxide.select("[O]", hydrogen="only")
+        peroxide.set_basis_name("6-31G", hydrogens[:1])
+        
+        s = geoprep.System([peroxide])
+        self.C.prepare_basis_data(s, options=options)
+        coordinates = self.C.make_tagged_cartesian_geometry(s, options=options)
+
+        u = Utility()
+        for j, c in enumerate(coordinates):
+            self.assertTrue(c.startswith(expected_tags[j]))
+            numbers = u.numericize(c, numeric_only=True)
+            self.assertEqual(3, len(numbers))
+
+    def test_create_geometry_basic(self):
+        #test generation of complete geometry block
+        btag = "basis_tag"
+        expected_tags = ["O0", "H0"]
+        options = {"basis_tag_name" : btag, "property_name" : btag}
+
+        peroxide = self.G.make_fragment("OO")
+        peroxide.set_basis_name("3-21G")
+        
+        s = geoprep.System([peroxide])
+        self.C.prepare_basis_data(s, options=options)
+        geometry = self.C.create_geometry(s, options=options)
+
+        for tag in expected_tags:
+            self.assertTrue(tag in geometry)
+
+    def test_create_geometry_c1_symmetry(self):
+        #test generation of geometry block with explicit C1 symmetry
+        btag = "basis_tag"
+        options = {"basis_tag_name" : btag, "property_name" : btag,
+                   "symmetry" : "C1"}
+
+        peroxide = self.G.make_fragment("OO")
+        peroxide.set_basis_name("3-21G")
+        
+        s = geoprep.System([peroxide])
+        self.C.prepare_basis_data(s, options=options)
+        geometry = self.C.create_geometry(s, options=options)
+
+        self.assertTrue("symmetry C1" in geometry)
+
+def runSuite(cls, verbosity=2, name=None):
+    """Run a unit test suite and return status code.
+
+    @param cls: class that the suite should be constructed from
+    @type cls : class
+    @param verbosity: verbosity level to pass to test runner
+    @type verbosity : int
+    @param name: name of a specific test in the suite to run
+    @type name : str
+    @return: unit test run status code
+    @rtype : int
+    """
+    try: 
+        if name:
+            suite = unittest.makeSuite(cls, name)
+        else:
+            suite = unittest.makeSuite(cls)
+            
+        return unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    
+    except SystemExit:
+        pass
+
+def runTests():
+    try:
+        test_name = sys.argv[1]
+        
+    except IndexError:
+        test_name = None
+
+    if test_name:
+        result = runSuite(Psi4TestCase, name = test_name)
+
+    else:
+        result = runSuite(Psi4TestCase)
+
+    return result
+
+if __name__ == '__main__':
+    runTests()
