b7e5d3e19181d432082b9dfadd803adfa07cff3f
Matt Ernst <mbernst@tricity.wsu.edu>
2015-04-22 23:40:01 -0700

    Added basic tests for ansible remote execution.

---
+++ tests/test_remote_execution.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 mode:python; tab-width:4; indent-tabs-mode:nil; py-indent-offset:4 -*-
+##
+
+"""
+    test_remote_execution
+    ~~~~~~~~~~~~~~
+
+    Test job execution over ansible, using ssh transport to 127.0.0.1. Use
+    NWChem and MOPAC 7 as back ends for test purposes because they are the
+    best supported via Debian packages.
+"""
+
+import sys
+import unittest
+import geoprep
+from adapters import nwchem, mopac7
+from sharedutilities import Utility
+
+class RETestCase(unittest.TestCase):
+    def setUp(self):
+        self.G = geoprep.Geotool()
+        self.C = nwchem.NWChem()
+        self.C2 = mopac7.Mopac7()
+
+    def tearDown(self):
+        pass
+
+    def test_nwchem_energy_scf_methane(self):
+        #very basic minimal basis set test for methane
+        expected_energy = -39.976642
+        methane = self.G.make_fragment("C")
+        methane.set_basis_name("3-21G")
+
+        #local execution
+        job = self.C.make_energy_job(methane, "hf:rhf")
+        job.run()
+        self.assertAlmostEqual(expected_energy, job.energy, places=5)
+
+        #over ansible
+        job2 = self.C.make_energy_job(methane, "hf:rhf")
+        job2.run(host="127.0.0.1")
+        self.assertAlmostEqual(expected_energy, job2.energy, places=5)
+
+    def test_nwchem_bad_input_error(self):
+        methane = self.G.make_fragment("C")
+        methane.set_basis_name("3-21G")
+        job = self.C.make_energy_job(methane, "hf:rhf")
+
+        #introduce an error in the input deck: misspell rhf as thf
+        job.deck = job.deck.replace("RHF", "THF")
+
+        #local execution
+        self.assertEqual("begin", job.runstate)
+        job.run()
+        self.assertEqual("error", job.runstate)
+
+        #over ansible
+        job2 = self.C.make_energy_job(methane, "hf:rhf")
+        job2.deck = job.deck.replace("RHF", "THF")
+        self.assertEqual("begin", job2.runstate)
+        job2.run(host="127.0.0.1")
+        self.assertEqual("error", job2.runstate)
+
+    def test_mopac_energy_pm3_methylium(self):
+        expected_energy = -5.641481
+        expected_hof = 0.408868
+        
+        methylium = self.G.make_system("[CH3+]")
+
+        #local execution
+        job = self.C2.make_energy_job(methylium, "semiempirical:pm3")
+        job.run()
+        self.assertAlmostEqual(expected_energy, job.energy, places=5)
+        self.assertAlmostEqual(expected_hof, job.heat_of_formation, places=5)
+
+        #over ansible
+        job2 = self.C2.make_energy_job(methylium, "semiempirical:pm3")
+        job2.run(host="127.0.0.1")
+        self.assertAlmostEqual(expected_energy, job.energy, places=5)
+        self.assertAlmostEqual(expected_hof, job.heat_of_formation, places=5)
+
+def runSuite(cls, verbosity=2, name=None):
+    """Run a unit test suite and return status code.
+
+    @param cls: class that the suite should be constructed from
+    @type cls : class
+    @param verbosity: verbosity level to pass to test runner
+    @type verbosity : int
+    @param name: name of a specific test in the suite to run
+    @type name : str
+    @return: unit test run status code
+    @rtype : int
+    """
+    try: 
+        if name:
+            suite = unittest.makeSuite(cls, name)
+        else:
+            suite = unittest.makeSuite(cls)
+            
+        return unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    
+    except SystemExit:
+        pass
+
+def runTests():
+    try:
+        test_name = sys.argv[1]
+        
+    except IndexError:
+        test_name = None
+
+    if test_name:
+        result = runSuite(RETestCase, name = test_name)
+
+    else:
+        result = runSuite(RETestCase)
+
+    return result
+
+if __name__ == '__main__':
+    runTests()
